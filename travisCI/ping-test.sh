1a7bc2115d15d0293b05e97fc64f9a4e3c21f8a3
Mitchell Krog <mitchellkrog@gmail.com>
2017-06-28 14:24:01 +0200

    Set Travis CI to use new ping test script.

--- travisCI/ping-test.sh
+++ travisCI/ping-test.sh
@@ -1,50 +1,56 @@
 #!/bin/bash
 # Ping Testing Script for Badd Boyz Hosts to check if Hosts file is working
-#result=$(ping -c 4 zx6.ru &> /dev/stderr)
-#if [ -n "$result" ]
-#then
-#  logger -t $LOGGER_TAG "Cannot create backup directory in $FINAL_BACKUP_DIR. Backup canceling: $result"
-#  exit 1
-#fi
-for I in {1..5}; do
+
+do_ping1() {
     if ping -nq -w5 -c1 000free.us | fgrep -q '127.0.0.1'; then
         echo "Hosts File Working"
-        exit 0
     else
         echo "Hosts File NOT Working"
         exit 1
-    fi
+	fi
+}
 
+do_ping2() {
     if ping -nq -w5 -c1 fix-website-errors.com | fgrep -q '127.0.0.1'; then
         echo "Hosts File Working"
-        exit 0
     else
         echo "Hosts File NOT Working"
         exit 1
-    fi
+	fi
+}
 
+do_ping3() {
     if ping -nq -w5 -c1 get-seo-domain.com | fgrep -q '127.0.0.1'; then
         echo "Hosts File Working"
-        exit 0
     else
         echo "Hosts File NOT Working"
         exit 1
-    fi
+	fi
+}
 
+do_ping4() {
     if ping -nq -w5 -c1 olympescort.com | fgrep -q '127.0.0.1'; then
         echo "Hosts File Working"
-        exit 0
     else
         echo "Hosts File NOT Working"
         exit 1
-    fi
+	fi
+}
 
+do_ping5() {
     if ping -nq -w5 -c1 zx6.ru | fgrep -q '127.0.0.1'; then
         echo "Hosts File Working"
-        exit 0
     else
         echo "Hosts File NOT Working"
         exit 1
-    fi
+	fi
+}
 
+do_ping1
+do_ping2
+do_ping3
+do_ping4
+do_ping5
+
+exit 0
 done
