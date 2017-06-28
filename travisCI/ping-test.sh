306eb8c4fc84219025279fe3e52da9cad2b39cd7
Mitchell Krog <mitchellkrog@gmail.com>
2017-06-28 12:37:44 +0200

    Final test of new ping-test script using multiple ping tests

--- travisCI/ping-test.sh
+++ travisCI/ping-test.sh
@@ -7,15 +7,43 @@
 #  exit 1
 #fi
 for I in {1..5}; do
-    if ping -nq -w5 -c1 zx6.ru | fgrep -q '127.0.0.1'; then
+    if ping -nq -w5 -c1 000free.us | fgrep -q '127.0.0.1'; then
+        echo "Hosts File Working"
         exit 0
     else
+        echo "Hosts File NOT Working"
+        exit 1
+    fi
+
+    if ping -nq -w5 -c1 fix-website-errors.com | fgrep -q '127.0.0.1'; then
+        echo "Hosts File Working"
+        exit 0
+    else
+        echo "Hosts File NOT Working"
         exit 1
     fi
 
     if ping -nq -w5 -c1 get-seo-domain.com | fgrep -q '127.0.0.1'; then
+        echo "Hosts File Working"
         exit 0
     else
+        echo "Hosts File NOT Working"
+        exit 1
+    fi
+
+    if ping -nq -w5 -c1 olympescort.com | fgrep -q '127.0.0.1'; then
+        echo "Hosts File Working"
+        exit 0
+    else
+        echo "Hosts File NOT Working"
+        exit 1
+    fi
+
+    if ping -nq -w5 -c1 zx6.ru | fgrep -q '127.0.0.1'; then
+        echo "Hosts File Working"
+        exit 0
+    else
+        echo "Hosts File NOT Working"
         exit 1
     fi
 
