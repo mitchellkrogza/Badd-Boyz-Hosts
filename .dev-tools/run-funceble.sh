ad734c434510fcd6fa1cc1af6b5d7f0f3e7c55bb
Mitchell Krog <mitchellkrog@gmail.com>
2017-08-27 14:03:04 +0200

    Hosts (+) - Funceble autosave set to 35 minutes.

--- .dev-tools/run-funceble.sh
+++ .dev-tools/run-funceble.sh
@@ -47,7 +47,7 @@ cd $TRAVIS_BUILD_DIR/.dev-tools/funceble/
 
 YEAR=$(date +%Y)
 MONTH=$(date +%m)
-sudo bash $TRAVIS_BUILD_DIR/.dev-tools/funceble/tool --autosave-minutes 20 --commit-autosave-message "Build Testing in Progress >> Funceble (Partial Build)" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
+sudo bash $TRAVIS_BUILD_DIR/.dev-tools/funceble/tool --autosave-minutes 35 --commit-autosave-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [funceble]" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
 
 # ************************************
 #  Run Funceble and Check Domains List
