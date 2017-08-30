66bc2e482c75f2b7bf8a7331a5bba53f815d2073
Mitchell Krog <mitchellkrog@gmail.com>
2017-08-30 17:28:54 +0200

    REBUILD: - New version of tool and new command line parameters for tool and funceble.

--- .dev-tools/run-funceble.sh
+++ .dev-tools/run-funceble.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# Fetch funceble script files and run a test
+# ********************
+# Run Funceble Testing
+# ********************
+
 # Created by: Mitchell Krog (mitchellkrog@gmail.com)
 # Copyright: Mitchell Krog - https://github.com/mitchellkrogza
 # Repo Url: https://github.com/mitchellkrogza/The-Big-List-of-Hacked-Malware-Web-Sites
@@ -19,13 +22,6 @@ _input=$TRAVIS_BUILD_DIR/PULL_REQUESTS/domains.txt
 # ******************************************
 
 sudo chown -R travis:travis $TRAVIS_BUILD_DIR/
-
-# *****************************************
-# Get latest versions of funceble from repo
-# *****************************************
-#sudo wget https://raw.githubusercontent.com/funilrys/funceble/master/funceble -O $TRAVIS_BUILD_DIR/.dev-tools/_funceble/funceble
-#sudo wget https://raw.githubusercontent.com/funilrys/funceble/master/tool -O $TRAVIS_BUILD_DIR/.dev-tools/_funceble/tool
-#sudo wget https://raw.githubusercontent.com/funilrys/funceble/master/iana-domains-db -O $TRAVIS_BUILD_DIR/.dev-tools/_funceble/iana-domains-db
 
 # *******************************
 # Set Funceble Scripts Executable
@@ -47,12 +43,12 @@ cd $TRAVIS_BUILD_DIR/.dev-tools/funceble/
 
 YEAR=$(date +%Y)
 MONTH=$(date +%m)
-sudo bash $TRAVIS_BUILD_DIR/.dev-tools/funceble/tool --autosave-minutes 35 --commit-autosave-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [funceble]" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
+sudo bash $TRAVIS_BUILD_DIR/.dev-tools/funceble/tool -u --dev --autosave-minutes 35 --commit-autosave-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [funceble]" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
 
 # ************************************
 #  Run Funceble and Check Domains List
 # ************************************
 
-sudo bash $TRAVIS_BUILD_DIR/.dev-tools/funceble/funceble --cmd-before-end "bash $TRAVIS_BUILD_DIR/.dev-tools/generate-hosts.sh && bash $TRAVIS_BUILD_DIR/.dev-tools/modify-readme.sh" --travis -a -ex -h -f $_input
+sudo bash $TRAVIS_BUILD_DIR/.dev-tools/funceble/funceble --cmd-before-end "bash $TRAVIS_BUILD_DIR/.dev-tools/generate-hosts.sh && bash $TRAVIS_BUILD_DIR/.dev-tools/modify-readme.sh" --travis -a -ex -h --plain --split -f $_input
 
 exit 0
