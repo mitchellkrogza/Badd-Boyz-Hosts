a8046478401b841f4089f7c20d4b2f7c2866f186
Mitchell Krog <mitchellkrog@gmail.com>
2017-10-03 10:39:11 +0200

    Re-Generate Hosts

--- .dev-tools/run-funceble.sh
+++ .dev-tools/run-funceble.sh
@@ -43,7 +43,7 @@ cd $TRAVIS_BUILD_DIR/.dev-tools/funceble/
 
 YEAR=$(date +%Y)
 MONTH=$(date +%m)
-sudo bash $TRAVIS_BUILD_DIR/.dev-tools/funceble/tool --dev -u --autosave-minutes 35 --commit-autosave-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [funceble]" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
+sudo bash $TRAVIS_BUILD_DIR/.dev-tools/funceble/tool --dev -u --autosave-minutes 20 --commit-autosave-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [funceble]" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
 
 # ************************************
 #  Run Funceble and Check Domains List
