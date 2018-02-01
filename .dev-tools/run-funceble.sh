38affade46a561c0bbd378d2b968157a6fbbda52
funilrys <contact@funilrys.com>
2018-02-01 11:51:19 +0100

    Fix issue mentionned into Travis CI
    Cf: https://travis-ci.org/mitchellkrogza/Badd-Boyz-Hosts/builds/336042656#L601
    
    Former-commit-id: 02ca47c4351124621d6d96df5393d495aa6727e3

--- .dev-tools/run-funceble.sh
+++ .dev-tools/run-funceble.sh
@@ -43,12 +43,12 @@ cd $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/
 
 YEAR=$(date +%Y)
 MONTH=$(date +%m)
-sudo bash $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/tool.py --dev -u && sudo bash $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/tool.py  --autosave-minutes 10 --commit-autosave-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [funceble]" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
+sudo $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/tool.py --dev -u && sudo bash $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/tool.py  --autosave-minutes 10 --commit-autosave-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [funceble]" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
 
 # ************************************
 #  Run Funceble and Check Domains List
 # ************************************
 
-sudo bash $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/PyFunceble.py --cmd-before-end "bash $TRAVIS_BUILD_DIR/.dev-tools/final-commit.sh" --travis --travis-branch 'dev' -a -ex --plain --split -f $_input
+sudo $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/PyFunceble.py --cmd-before-end "bash $TRAVIS_BUILD_DIR/.dev-tools/final-commit.sh" --travis --travis-branch 'dev' -a -ex --plain --split -f $_input
 
 exit 0
