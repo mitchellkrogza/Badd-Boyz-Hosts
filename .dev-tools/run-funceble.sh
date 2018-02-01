17a0f2937bcd3b0c8f95ac208774d933cb2f31cd
funilrys <contact@funilrys.com>
2018-02-01 11:45:36 +0100

    Introduction of PyFunceble architecture

--- .dev-tools/run-funceble.sh
+++ .dev-tools/run-funceble.sh
@@ -27,14 +27,14 @@ sudo chown -R travis:travis $TRAVIS_BUILD_DIR/
 # Set Funceble Scripts Executable
 # *******************************
 
-sudo chmod +x $TRAVIS_BUILD_DIR/.dev-tools/funceble/tool
-sudo chmod +x $TRAVIS_BUILD_DIR/.dev-tools/funceble/funceble
+sudo chmod +x $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/tool.py
+sudo chmod +x $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/funceble.py
 
 # ****************************
 # Switch to funceble directory
 # ****************************
 
-cd $TRAVIS_BUILD_DIR/.dev-tools/funceble/
+cd $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/
 
 # *************************
 # Run Funceble Install Tool
@@ -43,12 +43,12 @@ cd $TRAVIS_BUILD_DIR/.dev-tools/funceble/
 
 YEAR=$(date +%Y)
 MONTH=$(date +%m)
-sudo bash $TRAVIS_BUILD_DIR/.dev-tools/funceble/tool --dev -u --autosave-minutes 10 --commit-autosave-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [funceble]" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
+sudo bash $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/tool.py --dev -u && sudo bash $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/tool.py  --autosave-minutes 10 --commit-autosave-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [funceble]" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
 
 # ************************************
 #  Run Funceble and Check Domains List
 # ************************************
 
-sudo bash $TRAVIS_BUILD_DIR/.dev-tools/funceble/funceble --cmd-before-end "bash $TRAVIS_BUILD_DIR/.dev-tools/final-commit.sh" --travis -a -ex -h --plain --split -f $_input
+sudo bash $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/PyFunceble.py --cmd-before-end "bash $TRAVIS_BUILD_DIR/.dev-tools/final-commit.sh" --travis --travis-branch 'dev' -a -ex --plain --split -f $_input
 
 exit 0
