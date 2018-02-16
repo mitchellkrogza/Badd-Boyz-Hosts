44e416bc7df418d575a228ad7049c3e32c94a0bb
Travis <mitchellkrog@gmail.com>
2018-02-16 22:30:21 +0200

    V1.2018.02.2550 [PyFunceble]

--- .dev-tools/PyFunceble/PyFunceble.py
+++ .dev-tools/PyFunceble/PyFunceble.py
@@ -160,9 +160,9 @@ class Settings(object):  # pylint: disable=too-few-public-methods
     # Minimum of minutes before we start commiting to upstream under Travis CI.
     travis_autosave_minutes = 10
     # Default travis final commit message
-    travis_autosave_final_commit = "V1.2018.02.2549"
+    travis_autosave_final_commit = "V1.2018.02.2550"
     # Default travis commit message
-    travis_autosave_commit = "V1.2018.02.2549 [PyFunceble]"
+    travis_autosave_commit = "V1.2018.02.2550 [PyFunceble]"
     # Output into unified files.
     unified_file = True
     ##########################################################################
