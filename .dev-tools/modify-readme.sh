c8788564d2c129ce246111ee41293d043311cec7
Mitchell Krog <mitchellkrog@gmail.com>
2018-04-18 11:29:07 +0200

    Fix Variable Naming in Shell Scripts [PyFunceble skip]
    Former-commit-id: 75719228b435edd0c9bc83efe2dedb90a679e85c

--- .dev-tools/modify-readme.sh
+++ .dev-tools/modify-readme.sh
@@ -3,61 +3,39 @@
 # Created by: Mitchell Krog (mitchellkrog@gmail.com)
 # Copyright: Mitchell Krog - https://github.com/mitchellkrogza
 # Repo Url: https://github.com/mitchellkrogza/Badd-Boyz-Hosts
-
 # MIT License
-
-# Copyright (c) 2017 Mitchell Krog - mitchellkrog@gmail.com
-# https://github.com/mitchellkrogza
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
 # ******************
 # Set Some Variables
 # ******************
 
-YEAR=$(date +%Y)
-MONTH=$(date +%m)
-MY_GIT_TAG=V1.$YEAR.$MONTH.$TRAVIS_BUILD_NUMBER
-TOTAL_SITES=$(wc -l < $TRAVIS_BUILD_DIR/PULL_REQUESTS/domains.txt)
-ACTIVE_SITES=$(cat $TRAVIS_BUILD_DIR/.dev-tools/PyFunceble/output/logs/percentage/percentage.txt)
+yeartag=$(date +%Y)
+monthtag=$(date +%m)
+my_git_tag=V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER}
+total_sites=$(wc -l < ${TRAVIS_BUILD_DIR}/PULL_REQUESTS/domains.txt)
+active_sites=$(cat ${TRAVIS_BUILD_DIR}/.dev-tools/PyFunceble/output/logs/percentage/percentage.txt)
 
 # **********************************
 # Temporary database files we create
 # **********************************
 
-_inputdbA=/tmp/lastupdated.db
-_tmpfileA=tmpfileA
+inputdbA=/tmp/lastupdated.db
+tmpfileA=tmpfileA
 
 # ***************************************************************
 # Start and End Strings to Search for to do inserts into template
 # ***************************************************************
 
-_startmarker="_______________"
-_endmarker="____________________"
+startmarker="_______________"
+endmarker="____________________"
 
 # ****************************************
 # PRINT VERSION INFORMATION INTO README.md
 # ****************************************
 
-printf '%s\n%s%s%s\n%s%s\n```\n%s\n```\n%s' "$_startmarker" "[![VERSION](https://img.shields.io/badge/VERSION%20-%20" "$MY_GIT_TAG" "-blue.svg)](https://github.com/mitchellkrogza/Badd-Boyz-Hosts/commits/master)" "#### Bad Host Count: " "$TOTAL_SITES" "$ACTIVE_SITES" "$_endmarker" >> "$_tmpfileA"
-mv $_tmpfileA $_inputdbA
-ed -s $_inputdbA<<\IN
+printf '%s\n%s%s%s\n%s%s\n```\n%s\n```\n%s' "${startmarker}" "[![VERSION](https://img.shields.io/badge/VERSION%20-%20" "${my_git_tag}" "-blue.svg)](https://github.com/mitchellkrogza/Badd-Boyz-Hosts/commits/master)" "#### Bad Host Count: " "${total_sites}" "${active_sites}" "${endmarker}" >> "${tmpfileA}"
+mv ${tmpfileA} ${inputdbA}
+ed -s ${inputdbA}<<\IN
 1,/_______________/d
 /____________________/,$d
 ,d
@@ -68,16 +46,17 @@ ed -s $_inputdbA<<\IN
 w /home/travis/build/mitchellkrogza/Badd-Boyz-Hosts/README.md
 q
 IN
-rm $_inputdbA
+rm ${inputdbA}
 
 # ***********************
 # Truncate Cleaning Files
 # ***********************
 
-sudo truncate -s 0 $TRAVIS_BUILD_DIR/.input_sources/.False-Positives-Dead-Domains/whitelist-domains.txt
-sudo truncate -s 0 $TRAVIS_BUILD_DIR/.input_sources/.False-Positives-Dead-Domains/dead-domains.txt
+sudo truncate -s 0 ${TRAVIS_BUILD_DIR}/.input_sources/.False-Positives-Dead-Domains/whitelist-domains.txt
+sudo truncate -s 0 ${TRAVIS_BUILD_DIR}/.input_sources/.False-Positives-Dead-Domains/dead-domains.txt
 
-exit 0
+exit ${?}
+
 
 # MIT License
 
--- .dev-tools/pre-deploy.sh
+++ .dev-tools/pre-deploy.sh
@@ -3,37 +3,13 @@
 # Created by: Mitchell Krog (mitchellkrog@gmail.com)
 # Copyright: Mitchell Krog - https://github.com/mitchellkrogza
 # Repo Url: https://github.com/mitchellkrogza/Badd-Boyz-Hosts
-
 # MIT License
 
-# Copyright (c) 2017 Mitchell Krog - mitchellkrog@gmail.com
-# https://github.com/mitchellkrogza
+# ***************************************
+# Make sure we are in the Build Directory
+# ***************************************
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# ******************
-# Set Some Variables
-# ******************
-
-YEAR=$(date +"%Y")
-MONTH=$(date +"%m")
-cd $TRAVIS_BUILD_DIR
+cd ${TRAVIS_BUILD_DIR}
 
 # *******************************
 # Remove Remote Added by TravisCI
@@ -65,45 +41,45 @@ git checkout master
 # Make Sure Temp Files Exist
 # **************************
 
-sudo touch $TRAVIS_BUILD_DIR/.dev-tools/temp_combined-list.txt
+sudo touch ${TRAVIS_BUILD_DIR}/.dev-tools/temp_combined-list.txt
 
 # **********************************
 # Setup input bots and referer lists
 # **********************************
 
-_input1=$TRAVIS_BUILD_DIR/PULL_REQUESTS/domains.txt
+input1=${TRAVIS_BUILD_DIR}/PULL_REQUESTS/domains.txt
 
 # ***********************************
 # Make Sure Travis Owns All New Files
 # ***********************************
 
-sudo chown -R travis:travis $TRAVIS_BUILD_DIR
+sudo chown -R travis:travis ${TRAVIS_BUILD_DIR}
 
 # **************************************************************************
 # Sort lists alphabetically and remove duplicates before cleaning Dead Hosts
 # **************************************************************************
 
-sort -u $_input1 -o $_input1
+sort -u ${input1} -o ${input1}
 
 # *****************
 # Activate Dos2Unix
 # *****************
 
-dos2unix $_input1
+dos2unix ${input1}
 
 # ************************************
 # Make sure all scripts are executable
 # ************************************
 
-sudo chmod +x $TRAVIS_BUILD_DIR/.dev-tools/run-PyFunceble.sh
-sudo chmod +x $TRAVIS_BUILD_DIR/.dev-tools/modify-readme.sh
-sudo chmod +x $TRAVIS_BUILD_DIR/.dev-tools/generate-hosts.sh
+sudo chmod +x ${TRAVIS_BUILD_DIR}/.dev-tools/run-PyFunceble.sh
+sudo chmod +x ${TRAVIS_BUILD_DIR}/.dev-tools/modify-readme.sh
+sudo chmod +x ${TRAVIS_BUILD_DIR}/.dev-tools/generate-hosts.sh
 
 # ***************************************************
 # Run funceble to check for dead domains
 # ***************************************************
 
-sudo bash -x $TRAVIS_BUILD_DIR/.dev-tools/run-PyFunceble.sh
+sudo bash -x ${TRAVIS_BUILD_DIR}/.dev-tools/run-PyFunceble.sh
 
 
 exit ${?}
