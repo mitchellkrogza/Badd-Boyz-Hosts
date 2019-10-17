1159e7368d7ab2a23f4fbb958bacd1313a3d4985
Mitchell Krog <mitchellkrog@gmail.com>
2019-10-17 12:56:44 +0200

    Specify CONDA_ENVS_PATH

--- dev-tools/PrepareData.sh
+++ dev-tools/PrepareData.sh
@@ -34,6 +34,7 @@ checkforconda () {
 if conda 2>&1 | grep -i 'command not found'; then
    echo "${bold}${red}CONDA NOT FOUND - ${bold}${green}Installing Mini(Conda)"
    export PATH="${TRAVIS_BUILD_DIR}/miniconda/bin:${PATH}"
+   CONDA_ENVS_PATH="${TRAVIS_BUILD_DIR}/opt/anaconda/envs"
    sudo wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
    sudo bash miniconda.sh -b -p ${TRAVIS_BUILD_DIR}/miniconda
    hash -r
