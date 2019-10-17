581610fbf54f0e349aed812b9d963127cc137cb3
Mitchell Krog <mitchellkrog@gmail.com>
2019-10-17 13:28:22 +0200

    Force Commit with Conda

--- dev-tools/PrepareData.sh
+++ dev-tools/PrepareData.sh
@@ -131,6 +131,22 @@ WhiteListing () {
 }
 WhiteListing
 
+CommitData () {
+commitdate=$(date +%F)
+committime=$(date +%T)
+timezone=$(date +%Z)
+cd ${TRAVIS_BUILD_DIR}
+git remote rm origin
+git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_NAME}"
+git config --global push.default simple
+git checkout master
+git add -A
+git commit -am "V.${TRAVIS_BUILD_NUMBER} (${commitdate} ${committime} ${timezone}) [ci skip]"
+git push origin master    
+}
+CommitData
 
 exit ${?}
 
